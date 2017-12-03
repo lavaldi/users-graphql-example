a05a6a5411668842cc930478fd4f5b5a4b56a498
Claudia Valdivieso <claumavaldivieso@gmail.com>
2017-12-02 19:26:33 -0500

    add json-server with axios to fake data

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 
 const {
     GraphQLObjectType,
@@ -7,11 +7,6 @@ const {
     GraphQLInt,
     GraphQLSchema
 } = graphql;
-
-const users = [
-    { id: '23', firstName: 'Claudia', age: 27 },
-    { id: '47', firstName: 'Carlos', age: 27 }
-]
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -29,7 +24,8 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id });
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(resp => resp.data);
             }
         }
     }
