9a66b1c617b329c4dcf6940ac71b126438ffcbcb
Claudia Valdivieso <claumavaldivieso@gmail.com>
2017-12-02 20:59:31 -0500

    bidirectional relations company<->user

--- schema/schema.js
+++ schema/schema.js
@@ -5,21 +5,29 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
-        description: { type: GraphQLString }
-    }
+        description: { type: GraphQLString },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then(resp => resp.data);
+            }
+        }
+    })
 });
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
@@ -30,7 +38,7 @@ const UserType = new GraphQLObjectType({
                     .then(resp => resp.data);
             }
         }
-    }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
