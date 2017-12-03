8e0ab0b4c2f15b15c829b464818310ae15cfc828
Claudia Valdivieso <claumavaldivieso@gmail.com>
2017-12-02 19:31:43 -0500

    add nodemon to refesh the server

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const expressGraphQL = require('express-graphql');
 const schema = require('./schema/schema');
 
 const app = express();
+const port = 4000;
 
 app.use('/graphql', expressGraphQL({
     schema,
     graphiql: true
 }));
 
-app.listen(4000, () => {
-    console.log('Listening');
+app.listen(port, () => {
+    console.log(`Listening http://localhost:${port}`);
 });
